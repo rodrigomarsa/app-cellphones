3053b5f9335bddf5a185d8b475876106c1cdeac1
Rodrigo <rodrigomartins.agro@gmail.com>
2024-03-08 21:46:32 -0300

    fix: cors

--- client/src/services/Requests.js
+++ client/src/services/Requests.js
@@ -6,7 +6,6 @@ const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || 'http';
 const api = axios.create({
   baseURL: `${PROTOCOL}://${HOST}`,
   headers: {
-    'Content-Type': 'application/json',
     Authorization: '',
   },
 });
